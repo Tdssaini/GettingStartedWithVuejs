23132f7a554d1c7c37ca83be924f27b9e7fca248
Tarandeep <tarandeepsync@gmail.com>
2023-06-09 23:57:50 +0530

    Movie Booking app

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue';
+import MovieItem from "./components/MovieItem.vue";
+import { items } from "./assets/movies.json";
+import axios from 'axios'
+
+const movies = ref(items);
+
+function bookTicket(id) {
+  axios
+      .get('https://mocki.io/v1/a96e319c-d49d-433f-8c86-231bd1e39067?movieid='+id)
+      .then((response) => {
+          alert('Your ticket booking status is '+response.data.booked);
+      })
+}
+</script>
+
+<template>
+  <div class="app">
+    <div class="movie-list">
+        <MovieItem
+          v-for="movie in movies"
+          :key="movie.id"
+          :movie="movie"
+          @bookticket="bookTicket"
+        />
+      </div>
+  </div>
+</template>
---
+++ src/components/MovieItem.vue
@@ -0,0 +1,60 @@
+<script setup>
+const props = defineProps({
+  movie: { type: Object, default: null },
+  index: { type: Number, default: null },
+});
+
+const emit = defineEmits(["bookticket"]);
+
+function bookTicket(){
+  emit("bookticket", props.movie.id);
+}
+
+</script>
+
+<template>
+  <div class="movie-item">
+    <div class="movie-item-image-wrapper">
+      <img
+        v-if="movie?.image"
+        :src="movie.image"
+        class="movie-item-image"
+        :alt="movie.name"
+      />
+      <span v-else class="movie-item-no-image">
+        <span class="text-4xl text-white">No image</span>
+      </span>
+    </div>
+
+    <div >
+      <div >
+        <h3 >{{ movie.name }}</h3>
+        <div class="movie-item-genres-wrapper">
+          <span
+            v-for="genre in movie.genres"
+            :key="`${movie.id}-${genre}`"
+            class="movie-item-genre-tag"
+            >{{ genre }}</span
+          >
+        </div>
+      </div>
+      <div class="movie-item-description-wrapper">
+        <p class="movie-item-description">{{ movie.description }}</p>
+      </div>
+      <div class="movie-item-rating-wrapper">
+        <span class="movie-item-rating-text">
+          Rating: ({{ movie.rating }}/5)
+        </span>
+      </div>
+    </div>
+    <button class="greenButton" @click="bookTicket">Book Tickets</button>
+  </div>
+</template>
+<style scoped>
+    .greenButton{
+      color:white;
+      background: green;
+      padding:5px;
+      margin-top:20px;
+    }
+</style>
